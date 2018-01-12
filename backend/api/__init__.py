729b890fa8215137e78153432490140ef97dafd2
Joshua <KickinWingAnimalDoctor@users.noreply.github.com>
2018-01-11 21:17:06 -0500

    Update __init__.py

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -4,43 +4,94 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 
-db_connect = create_engine('sqlite:////home/kickinwing/Solar-Weather-Station/backend/weatherdatabase.db')
+db_connect = create_engine('sqlite:////home/di0de/Solar-Weather-Station/backend/weatherdatabase.db')
 app = Flask(__name__)
 api = Api(app)
 
-class Strikes(Resource):
+class NodesList(Resource):
     def get(self):
-        conn = db_connect.connect() 
-        query = conn.execute("select tdate, distance from strikes") 
+        conn = db_connect.connect()
+        query = conn.execute("select * from nodes")
         return {'strikes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor.fetchall()]}
-
 
     def post(self):
         conn = db_connect.connect()
         print(request.json)
-        tdate = request.json['tdate']
-        distance = request.json['distance']
-        query = conn.execute("insert into strikes values('{0}','{1}')".format(tdate,distance))
+        nodeid = request.json['nodeid']
+        location = request.json['location']
+        status = request.json['status']
+        dateadded = request.json['dateadded']
+        query = conn.execute("insert into nodes values('{0}', '{1}', '{2}', '{3}')".format(nodeid, location, status, dateadded))
         return {'status':'success'}
 
-class Multidata(Resource):
-    def get(self):
+class Nodes(Resource):
+    def get(self, nodeid):
         conn = db_connect.connect()
-        query = conn.execute("select tdate, humidity, pressure, temperature from multisensor;")
+        query = conn.execute("select * from nodes where nodeid = " + nodeid + ";")
+        return {nodeid : [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor.fetchall()]}
+
+    def delete(self, nodeid):
+        conn = db_connect.connect()
+        query = conn.execute("delete from nodes where nodeid = " + nodeid + ";")
+        return {'status':'succcess'}
+
+
+class Strikes(Resource):
+    def get(self, nodeid):
+        conn = db_connect.connect()
+        query = conn.execute("select ddate, distance from strikes where nodeid = " + nodeid + ";")
+        return {'strikes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor.fetchall()]}
+
+
+    def post(self, nodeid):
+        conn = db_connect.connect()
+        print(request.json)
+        date = request.json['date']
+        distance = request.json['distance']
+        query = conn.execute("insert into strikes values('{0}','{1}', '{2}')".format(nodeid, date, distance))
+        return {'status':'success'}
+
+
+class Multidata(Resource):
+    def get(self, nodeid):
+        conn = db_connect.connect()
+        query = conn.execute("select tdate, humidity, pressure, temperature from multisensor where nodeid = " + nodeid + ";")
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
+
+    def post(self, nodeid):
+        conn = db_connect.connect()
+        print(request.json)
+        date = request.json['date']
+        humidity = request.json['humidity']
+        pressure = request.json['pressure']
+        temperature = request.json['temperature']
+        query = conn.execute("insert into multisensor values('{0}','{1}', '{2}', '{3}', '{4}')".format(nodeid, date, humidity, pressure, temperature))
+        return {'status':'success'}
 
 class Power(Resource):
-    def get(self):
+    def get(self, nodeid):
         conn = db_connect.connect()
-        query = conn.execute("select * from power;")
+        query = conn.execute("select * from power " + nodeid + ";")
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
+    def post(self, nodeid):
+        conn = db_connect.connect()
+        print(request.json)
+        date = request.json['date']
+        volts = request.json['volts']
+        amps = request.json['amps']
+        watts = request.json['watts']
+        query = conn.execute("insert into strikes values('{0}','{1}', '{2}', '{3}', '{4}')".format(nodeid, date, volts, amps, watts))
+        return {'status':'success'}
 
-api.add_resource(Strikes, '/strikes') 
-api.add_resource(Multidata, '/multidata')
-api.add_resource(Power, '/power')
+
+api.add_resources(NodesList, '/nodes')
+api.add_resources(Nodes, '/nodes/<nodeid>')
+api.add_resource(Strikes, '/nodes/<nodeid>/strikes')
+api.add_resource(Multidata, '/nodes/<nodeid>/multidata')
+api.add_resource(Power, '/nodes/<nodeid>/power')
 
 
 if __name__ == '__main__':
