33d8a2ba61046574df7301a5819e5af09773b76d
Joshua <KickinWingAnimalDoctor@users.noreply.github.com>
2018-01-13 02:28:46 -0500

    Update sql.php

--- html/sql.php
+++ html/sql.php
@@ -1,11 +1,12 @@
-<?PHP 
-  $db = new SQLite3('/home/pi/WeatherDashboard/weatherdatabase.db');
+<?PHP
+  $db = new SQLite3('/home/kickinwing/Solar-Weather-Station/backend/weatherdatabase.db');
   $temp = $db->query('SELECT temperature, max(tdate) FROM multisensor;');
   $strikes = $db->query('SELECT COUNT(*) FROM strikes WHERE tdate >= datetime(\'now\', \'-1 day\');');
   $strikeD = $db->query('SELECT * FROM strikes LIMIT 15 OFFSET (SELECT COUNT(*) FROM strikes)-15;');
   $humidity = $db->query('SELECT humidity, max(tdate) from multisensor;');
   $pressure = $db->query('SELECT pressure, max(tdate) FROM multisensor;');
   $volts = $db->query('SELECT bvolts, max(tdate) from power;');
+  $nodes = $db->query('SELECT * FROM nodes;');
   $bvolts = $volts->fetchArray();
   $pressurepascals = $pressure->fetchArray();
   $humiditypercent = $humidity->fetchArray();
